c4528e9dd77bf8be42ed2948b84c439fdafd1b7c
sayang10 <71965204+sayang10@users.noreply.github.com>
2020-11-26 17:19:25 +0800

    Add files via upload

---
+++ oke.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+clear
+echo -e "\e[0;33mWelcome bro.."
+echo "Silahkan di nikmati yahh"
+sleep 4
+clear
+echo -e "\e[0;34mSilahkan di isi yah bro"
+read -p "Nama Author    : " nama 
+read -p "Nama Facebook  : " fb
+read -p "Nomor Whatsapp : " wa
+sleep 2
+echo -e "\e[0;32mOke berhasil cuk"
+sleep 2
+clear
+echo -e "\e[0;33m██████████████████████████████████████████████"
+echo -e "\e[0;33m█ \e[0;31mAuthor   :\e[0;90m " $nama
+echo -e "\e[0;33m█ \e[0;31mTeam     :\e[0;90m  TERMUX-ID"
+echo -e "\e[0;33m█ \e[0;31mFacebook :\e[0;90m " $fb
+echo -e "\e[0;33m█ \e[0;31mWhatsapp :\e[0;90m " $wa
+echo -e "\e[0;33m██████████████████████████████████████████████"
+echo -e "\e[0;34mMasukkan no target di bawah ini"
+echo -e "\e[0;90mContoh : 628××××××××× \e[0;31m "
+read -p "No target : " target
+sleep 3
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2
+echo "Oke berhasil mengirim ke no ini " $target
+sleep 2 
+echo "Makasih yah sudah mencoba script kami"
+sleep 3
+clear
